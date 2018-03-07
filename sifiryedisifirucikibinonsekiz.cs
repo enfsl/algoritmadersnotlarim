7749c04e732df8732746b4bee563390af4f4e410
Emre Nefesli <emrenefesli@outlook.com>
2018-03-07 11:14:47 +0300

    1

--- sifiryedisifirucikibinonsekiz.cs
+++ sifiryedisifirucikibinonsekiz.cs
@@ -48,3 +48,53 @@ namespace Application
         }
     }
 }
+
+/**/
+using System;
+
+namespace Application
+{
+    class Ornek
+    {
+        static void Test(int a,int b)
+        {
+            Console.WriteLine("int'li metot çalıştı");
+        }
+        static void Test(byte a, byte b)
+        {
+            Console.WriteLine("byte'li metot çalıştı");
+        }
+        static void Main(string[] args)
+        {
+            Test(33,44);
+            Test(333,443);
+        }
+    }
+}
+
+/*bilinçsiz tip dönüşümü var
+int byte ve short'u kapsadığı için 3'üncüye de int yazdıracak*/
+using System;
+
+namespace Application
+{
+    class Ornek
+    {
+        static void Test(int a,int b)
+        {
+            Console.WriteLine("int'li metot çalıştı");
+        }
+        static void Test(string a, string b)
+        {
+            Console.WriteLine("string'li metot çalıştı");
+        }
+        static void Main(string[] args)
+        {
+            Test(3,4);
+            Test("Prog","tek.");
+            byte a = 5;
+            short b = 10;
+            Test(a, b);
+        }
+    }
+}
